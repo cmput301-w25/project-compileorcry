47ced8cec6e8371624b859a5f7a000767af79339
Matthew Naruzny <matthew@mnaruzny.com>
2025-02-27 02:36:04 -0700

    Add dependencies for UI testing and added tests for Login/Signup fragments

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -51,4 +51,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-firestore:25.1.2")
     implementation ("com.firebase:geofire-android-common:3.1.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.uiautomator:uiautomator:2.2.0")
 }
