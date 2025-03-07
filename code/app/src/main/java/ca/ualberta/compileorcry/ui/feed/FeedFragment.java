88be4b1045ba7619305fb64bdbefd38c66f8cb69
Jasmine <jamah2@ualberta.ca>
2025-03-06 18:35:52 -0700

    arrrghghjhglsjhg

--- code/app/src/main/java/ca/ualberta/compileorcry/ui/feed/FeedFragment.java
+++ code/app/src/main/java/ca/ualberta/compileorcry/ui/feed/FeedFragment.java
@@ -4,34 +4,66 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import ca.ualberta.compileorcry.databinding.FragmentFeedBinding;
+import ca.ualberta.compileorcry.domain.models.User;
+import ca.ualberta.compileorcry.features.mood.data.MoodList;
+import ca.ualberta.compileorcry.features.mood.data.QueryType;
 
 public class FeedFragment extends Fragment {
+    // private FragmentFeedBinding binding;
+    private FeedViewModel feedViewModel;
+    private MoodListAdapter adapter;
+    private RecyclerView recyclerView;
+    private Spinner filterSpinner;
 
-    private FragmentFeedBinding binding;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_feed, container, false);
+        Spinner filterSpinner = view.findViewById(R.id.filter_spinner);
+        User user = User.getActiveUser();
+        recyclerView = view.findViewById(R.id.feed_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireContext())); // get vs require context?
+//        adapter = new MoodListAdapter(new MoodList(user, ));
+//        recyclerView.setAdapter(adapter);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        FeedViewModel feedViewModel =
-                new ViewModelProvider(this).get(FeedViewModel.class);
+        FeedViewModel feedViewModel = new ViewModelProvider(this).get(FeedViewModel.class);
+        loadFeed();
 
-        binding = FragmentFeedBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        return view;
+    }
 
-        final TextView textView = binding.textFeed;
-        feedViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    private void loadFeed() {
+        if (User.getActiveUser() != null) {
+            MoodList.createMoodList(User.getActiveUser(), QueryType.FOLLOWING, new MoodList.MoodListListener() {
+                @Override
+                public void returnMoodList(MoodList moodList) {
+                    if (feedViewModel != null) {
+                        feedViewModel.setMoodEvents(moodList.getMoodEvents());
+                    }
+                }
+
+                @Override
+                public void updatedMoodList() {
+                    // Handled automatically
+                }
+            }, null);
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        // binding = null;
     }
 }
--- code/app/src/main/java/ca/ualberta/compileorcry/ui/feed/FeedViewModel.java
+++ code/app/src/main/java/ca/ualberta/compileorcry/ui/feed/FeedViewModel.java
@@ -1,19 +1,61 @@
 package ca.ualberta.compileorcry.ui.feed;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class FeedViewModel extends ViewModel {
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-    private final MutableLiveData<String> mText;
+import java.util.ArrayList;
+import java.util.List;
 
-    public FeedViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is 'feed' fragment");
+import ca.ualberta.compileorcry.domain.models.User;
+import ca.ualberta.compileorcry.features.mood.data.MoodList;
+import ca.ualberta.compileorcry.features.mood.data.QueryType;
+import ca.ualberta.compileorcry.features.mood.model.MoodEvent;
+
+public class FeedViewModel extends ViewModel{
+
+    private final MutableLiveData<MoodList> moodListLiveData;
+    private MoodList moodList;
+    private FirebaseFirestore db;
+    private User user;
+    private QueryType filter = null;
+    public FeedViewModel(User user) {
+        moodListLiveData = new MutableLiveData<>();
+        db = FirebaseFirestore.getInstance();
+        loadMoodList();
+    }
+    public LiveData<MoodList> getMoodListLiveData() {
+        return moodListLiveData;
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    private void setMoodList(MoodList moodList) {
+        this.moodList = moodList;
     }
+    public void setFilter(QueryType filter) {
+        this.filter = filter;
+        loadMoodList();
+    }
+    private void loadMoodList() {
+        db.collection("most_recent_moods").get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        List<MoodEvent> moodEvents = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            MoodEvent moodEvent = document.toObject(MoodEvent.class);
+                            moodEvent.setIdFromDocRef(document.getReference());
+                            moodEvents.add(moodEvent);
+                        }
+                        moodList.setMoodEvents(moodEvents);
+                        moodListLiveData.setValue(moodList);
+                    } else {
+                        Log.e("FeedViewModel", "Error getting documents: ", task.getException());
+                    }
+                });
+    }
+
 }
