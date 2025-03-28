ea00ad974197733c689630191c20fe0d890b606b
Matthew Naruzny <matthew@mnaruzny.com>
2025-03-28 17:55:41 -0600

    Add ability to follow, unfollow, and cancel follow request from View Profile Fragment

--- code/app/src/main/java/ca/ualberta/compileorcry/ui/view_profile/ViewProfileFragment.java
+++ code/app/src/main/java/ca/ualberta/compileorcry/ui/view_profile/ViewProfileFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import ca.ualberta.compileorcry.R;
 import ca.ualberta.compileorcry.databinding.FragmentViewProfileBinding;
 import ca.ualberta.compileorcry.domain.models.User;
+import ca.ualberta.compileorcry.features.mood.model.FollowHelper;
 
 
 public class ViewProfileFragment extends Fragment {
@@ -20,6 +21,26 @@ public class ViewProfileFragment extends Fragment {
     private FragmentViewProfileBinding binding;
 
     private User displayUser;
+
+    /*
+    Refreshes the labels on the follow/unfollow/requested button
+     */
+    private void refreshFollowStatus(){
+        try {
+            if(FollowHelper.isUserFollowing(User.getActiveUser().getUsername(), displayUser.getUsername())){ // User is following user
+                binding.followButtonText.setText(R.string.button_unfollow);
+                binding.followButton.setIconResource(R.drawable.ic_friends_40dp);
+            } else if (FollowHelper.hasUserRequestedFollow(User.getActiveUser().getUsername(), displayUser.getUsername())) { // User has requested to follow
+                binding.followButtonText.setText(R.string.button_requested);
+                binding.followButton.setIconResource(R.drawable.ic_history_40dp);
+            } else { // User is not following and has not requested
+                binding.followButtonText.setText(R.string.button_follow);
+                binding.followButton.setIconResource(R.drawable.ic_person_24dp);
+            }
+        } catch (InterruptedException e){
+            Log.e("ViewProfile", "Error while checking follow status");
+        }
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,24 +53,36 @@ public class ViewProfileFragment extends Fragment {
         String displayProfileUsername = ViewProfileFragmentArgs.fromBundle(getArguments()).getProfileUsername();
         Log.d("ViewProfile", ("Displaying: " + displayProfileUsername));
         User.get_user(displayProfileUsername, (user, error) -> {
-            if(error != null){
+            if(error != null){ // Display error message on error fetching user
                 binding.viewProfileName.setText(R.string.error_loading_user);
                 binding.viewProfileUsername.setText(null);
                 return;
             }
             displayUser = user;
-            binding.viewProfileUsername.setText(user.getUsername());
-            binding.viewProfileName.setText(user.getName());
-
+            // Update UI with user info
+            binding.viewProfileUsername.setText(displayUser.getUsername());
+            binding.viewProfileName.setText(displayUser.getName());
             binding.followButton.setEnabled(true);
-            //TODO: Add displaying profile photo
+
+            refreshFollowStatus();
         });
 
 
         // Create button event handlers
         binding.followButton.setOnClickListener((l) -> {
             if(displayUser != null){
-                //TODO: Add functionality
+                try {
+                    if(FollowHelper.isUserFollowing(User.getActiveUser().getUsername(), displayUser.getUsername())){ // User is following user
+                        FollowHelper.unfollowUser(User.getActiveUser().getUsername(), displayUser.getUsername());
+                    } else if (FollowHelper.hasUserRequestedFollow(User.getActiveUser().getUsername(), displayUser.getUsername())) { // User has requested to follow
+                        FollowHelper.handleFollowRequest(displayUser, User.getActiveUser().getUsername(), false); // Deny follow request to cancel it
+                    } else { // User is not following and has not requested
+                        FollowHelper.createFollowRequest(User.getActiveUser(), displayUser.getUsername());
+                    }
+                } catch (InterruptedException e) {
+                    Log.e("ViewProfile", "Error while handling follow button press");
+                }
+                refreshFollowStatus();
             }
         });
 
