709e03a8de09f9ae41f7dfdab9ef628c6f1f1574
Balpreet Singh <bjuneja@ualberta.ca>
2025-03-31 05:11:11 -0600

    ALl tests work

--- code/app/src/androidTest/java/ca/ualberta/compileorcry/MoodInfoDialogTest.java
+++ code/app/src/androidTest/java/ca/ualberta/compileorcry/MoodInfoDialogTest.java
@@ -49,37 +49,30 @@ public class MoodInfoDialogTest {
         SystemClock.sleep(2000);
     }
 
-    private void createMoodEvent(String state, String trigger, String situation) {
+    private void createMoodEvent() {
         onView(withId(R.id.navigation_new)).perform(click());
 
 
 
-        // Set emotional state directly
         onView(withId(R.id.new_event_emotional_state_autocomplete))
-                .perform(replaceText(state), closeSoftKeyboard());
+                .perform(replaceText("Happiness"));
 
-        onView(withId(R.id.new_event_date_text)).perform(click());
-        onView(withText("OK")).perform(click());
-        onView(withText("OK")).perform(click());
+        onView(withId(R.id.new_event_date_text))
+                .perform(replaceText("2020-10-01 @ 12:01"));
 
-        // Set trigger text
         onView(withId(R.id.new_event_trigger_text))
-                .perform(replaceText(trigger), closeSoftKeyboard());
+                .perform(typeText("Music"), closeSoftKeyboard());
 
-        // Set social situation directly
         onView(withId(R.id.new_event_social_situation_autocomplete))
-                .perform(replaceText(situation), closeSoftKeyboard());
+                .perform(replaceText("Alone"));
 
-        // Submit
         onView(withId(R.id.create_button)).perform(click());
-
-        SystemClock.sleep(1500); // Wait for navigation back
     }
 
     @Test
     public void testViewMoodEventFieldsVisibleInFollowing() {
         performLoginAndGoToFeed();
-        createMoodEvent("Anger", "sunlight", "Alone");
+        createMoodEvent();
         onView(withId(R.id.back_button)).perform(click());
         onView(withId(R.id.feed_spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("History"))).perform(click());
@@ -90,36 +83,39 @@ public class MoodInfoDialogTest {
         onData(allOf(is(instanceOf(String.class)), is("Following"))).perform(click());
         SystemClock.sleep(1000);
 
-        onView(withText("Anger")).perform(click());
+        onView(withText("Happiness")).perform(click());
 
         // Assert that the mood details are correctly displayed
-        onView(withId(R.id.moodinfo_state_text)).check(matches(withText("Anger")));
-        onView(withId(R.id.moodinfo_trigger_display)).check(matches(withText("sunlight")));
+        onView(withId(R.id.moodinfo_state_text)).check(matches(withText("Happiness")));
+        onView(withId(R.id.moodinfo_trigger_display)).check(matches(withText("Music")));
         onView(withId(R.id.moodinfo_situation_text)).check(matches(withText("Alone")));
     }
 
     @Test
     public void testEditMoodEventUpdatesProperly() {
         performLoginAndGoToFeed();
-        createMoodEvent("Anger", "sunlight", "Alone");
+        createMoodEvent();
         onView(withId(R.id.back_button)).perform(click());
         onView(withId(R.id.feed_spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("History"))).perform(click());
 
 
-        onView(withText("Anger")).perform(click());
+        onView(withText("Happiness")).perform(click());
 
-        onView(withId(R.id.moodinfo_state_auto_complete)).perform(replaceText("Happy"));
+        onView(withId(R.id.moodinfo_state_auto_complete)).perform(replaceText("Anger"));
         onView(withId(R.id.moodinfo_trigger_text)).perform(replaceText("TestTrigger"));
         onView(withId(R.id.moodinfo_situation_auto_complete)).perform(replaceText("Alone"));
         onView(withId(R.id.save_button)).perform(click());
 
         SystemClock.sleep(2000);
-
+        onView(withId(R.id.feed_spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is("Following"))).perform(click());
+        SystemClock.sleep(2000);
         onView(withText("Anger")).perform(click());
+        SystemClock.sleep(2000);
 
         // Assert that the mood details are correctly displayed
-        onView(withId(R.id.moodinfo_state_text)).check(matches(withText("Happy")));
+        onView(withId(R.id.moodinfo_state_text)).check(matches(withText("Anger")));
         onView(withId(R.id.moodinfo_trigger_display)).check(matches(withText("TestTrigger")));
         onView(withId(R.id.moodinfo_situation_text)).check(matches(withText("Alone")));
     }
@@ -127,12 +123,13 @@ public class MoodInfoDialogTest {
     @Test
     public void testDeleteMoodEventRemovesFromHistory() {
         performLoginAndGoToFeed();
-
+        createMoodEvent();
+        onView(withId(R.id.back_button)).perform(click());
         onView(withId(R.id.feed_spinner)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("History"))).perform(click());
-        createMoodEvent("Scared", "loud sound", "Alone");
 
-        onView(withId(R.id.recyclerViewMoodHistory)).perform(click());
+
+        onView(withText("Happiness")).perform(click());
         onView(withId(R.id.delete_button)).perform(click());
 
         SystemClock.sleep(1500);
